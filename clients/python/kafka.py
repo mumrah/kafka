f7806828d1ca36b124bbaadf48de86559fd34aba
David Arthur <mumrah@gmail.com>
2012-09-24 08:56:25 -0400

    Add logging and example script
    Also updated docs to be consistent with Kafka docs (e.g., int16 instead
    of short)

--- clients/python/kafka.py
+++ clients/python/kafka.py
@@ -1,8 +1,11 @@
 from collections import namedtuple
+import logging
 import select
 import socket
 import struct
 import zlib
+
+log = logging.getLogger("org.apache.kafka")
 
 def length_prefix_message(msg):
     """
@@ -23,12 +26,12 @@ error_codes = {
     4: "InvalidFetchSize"
 }
 
-class RequestKey(object):
-    PRODUCE = 0
-    FETCH = 1
-    MULTIFETCH = 2
-    MULTIPRODUCE = 3
-    OFFSET = 4
+class KafkaException(Exception):
+    def __init__(self, errorType):
+        self.errorType = errorType
+    def __str__(self):
+        return str(errorType)
+
 
 Message = namedtuple("Message", ["magic", "attributes", "crc", "payload"])
 FetchRequest = namedtuple("FetchRequest", ["topic", "partition", "offset", "size"])
@@ -41,7 +44,7 @@ class KafkaClient(object):
     =================
 
     <Request>     ::= <len> <request-key> <payload>
-    <len>         ::= <int>
+    <len>         ::= <int32>
     <request-key> ::= 0 | 1 | 2 | 3 | 4
     <payload>     ::= <ProduceRequest> | <FetchRequest> | <MultiFetchRequest> | <MultiProduceRequest> | <OffsetRequest>
 
@@ -49,39 +52,53 @@ class KafkaClient(object):
     ==================
 
     <Response>    ::= <len> <err> <payload>
-    <len>         ::= <int>
+    <len>         ::= <int32>
     <err>         ::= -1 | 0 | 1 | 2 | 3 | 4
     <payload>     ::= <ProduceResponse> | <FetchResponse> | <MultiFetchResponse> | <MultiProduceResponse> | <OffsetResponse>
 
     Messages are big-endian byte order
     """
 
+    PRODUCE_KEY      = 0
+    FETCH_KEY        = 1
+    MULTIFETCH_KEY   = 2
+    MULTIPRODUCE_KEY = 3
+    OFFSET_KEY       = 4
+
     def __init__(self, host, port):
         self.host = host
         self.port = port
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._sock.connect((host, port))
+        log.debug("Connected to %s on %d", host, port)
+
+    ######################
+    #   Protocol Stuff   #
+    ######################
 
     def _consume_response_iter(self):
         """
         This method handles the response header and error messages. It 
         then returns an iterator for the chunks of the response
         """
+        log.debug("Handling response from Kafka")
         # Header
         resp = self._sock.recv(6)
         if resp == "":
             raise Exception("Got no response from Kafka")
         (size, err) = struct.unpack('>iH', resp)
 
+        log.debug("About to read %d bytes from Kafka", size-2)
         # Handle error
         error = error_codes.get(err)
         if error is not None:
-            raise Exception(error) 
+            raise KafkaException(error)
 
         # Response iterator
         total = 0
         while total < (size-2):
             resp = self._sock.recv(1024)
+            log.debug("Read %d bytes from Kafka", len(resp))
             if resp == "":
                 raise Exception("Underflow")
             total += len(resp)
@@ -110,11 +127,12 @@ class KafkaClient(object):
         <Message>    ::= <Message-0> | <Message-1> 
         <Message-0>  ::= <N> 0 <header-0> <payload>
         <Message-1>  ::= <N> 1 <header-1> <payload>
-        <N>          ::= <int>
+        <N>          ::= <int32>
         <header-0>   ::= <crc>
         <header-1>   ::= <attributes><crc>
+        <crc>        ::= <int32>
         <payload>    ::= <bytes>
-        <attributes> ::= <byte> 
+        <attributes> ::= <int8> 
 
         The crc is a crc32 checksum of the message payload. The attributes are bitmask
         used for indicating the compression algorithm.
@@ -144,20 +162,20 @@ class KafkaClient(object):
         <ProduceRequest> ::= <request-key> <topic> <partition> <len> <MessageSet>
         <request-key>    ::= 0
         <topic>          ::= <topic-length><string>
-        <topic-length>   ::= <short>
-        <partition>      ::= <int>
-        <len>            ::= <int>
+        <topic-length>   ::= <int16>
+        <partition>      ::= <int32>
+        <len>            ::= <int32>
 
-        The request-key (0) is encoded as a short. len is the length of the proceeding MessageSet
+        The request-key (0) is encoded as a short (int16). len is the length of the proceeding MessageSet
         """
         (topic, partition, messages) = produceRequest
         message_set = self.create_message_set(messages)
         req = struct.pack('>HH%dsii%ds' % (len(topic), len(message_set)), 
-                RequestKey.PRODUCE, len(topic), topic, partition, len(message_set), message_set)
+                KafkaClient.PRODUCE_KEY, len(topic), topic, partition, len(message_set), message_set)
         return req
 
     def create_multi_produce_request(self, produceRequests):
-        req = struct.pack('>HH', RequestKey.MULTIPRODUCE, len(produceRequests))
+        req = struct.pack('>HH', KafkaClient.MULTIPRODUCE_KEY, len(produceRequests))
         for (topic, partition, messages) in produceRequests:
             message_set = self.create_message_set(messages)
             req += struct.pack('>H%dsii%ds' % (len(topic), len(message_set)), 
@@ -173,16 +191,16 @@ class KafkaClient(object):
         <FetchRequest> ::= <request-key> <topic> <partition> <offset> <size>
         <request-key>  ::= 1
         <topic>        ::= <topic-length><string>
-        <topic-length> ::= <short>
-        <partition>    ::= <int>
-        <offset>       ::= <long>
-        <size>         ::= <int>
+        <topic-length> ::= <int16>
+        <partition>    ::= <int32>
+        <offset>       ::= <int64>
+        <size>         ::= <int32>
     
-        The request-key (1) is encoded as a short.
+        The request-key (1) is encoded as a short (int16).
         """
         (topic, partition, offset, size) = fetchRequest
         req = struct.pack('>HH%dsiqi' % len(topic), 
-                RequestKey.FETCH, len(topic), topic, partition, offset, size)
+                KafkaClient.FETCH_KEY, len(topic), topic, partition, offset, size)
         return req
 
     def create_multi_fetch_request(self, fetchRequests):
@@ -201,18 +219,18 @@ class KafkaClient(object):
         ===========
         <MultiFetchRequest> ::= <request-key> <num> [ <FetchRequests> ]
         <request-key>       ::= 2
-        <num>               ::= <short>
+        <num>               ::= <int16>
         <FetchRequests>     ::= <FetchRequest> [ <FetchRequests> ]
         <FetchRequest>      ::= <topic> <partition> <offset> <size>
         <topic>             ::= <topic-length><string>
-        <topic-length>      ::= <short>
-        <partition>         ::= <int>
-        <offset>            ::= <long>
-        <size>              ::= <int>
+        <topic-length>      ::= <int16>
+        <partition>         ::= <int32>
+        <offset>            ::= <int64>
+        <size>              ::= <int32>
 
-        The request-key (2) is encoded as a short.
+        The request-key (2) is encoded as a short (int16).
         """
-        req = struct.pack('>HH', RequestKey.MULTIFETCH, len(fetchRequests))
+        req = struct.pack('>HH', KafkaClient.MULTIFETCH_KEY, len(fetchRequests))
         for (topic, partition, offset, size) in fetchRequests:
             req += struct.pack('>H%dsiqi' % len(topic), len(topic), topic, partition, offset, size)
         return req
@@ -226,16 +244,16 @@ class KafkaClient(object):
         <OffsetRequest> ::= <request-key> <topic> <partition> <time> <max-offsets>
         <request-key>   ::= 4
         <topic>         ::= <topic-length><string>
-        <topic-length>  ::= <short>
-        <partition>     ::= <int>
+        <topic-length>  ::= <int16>
+        <partition>     ::= <int32>
         <time>          ::= <epoch>
-        <epoch>         ::= <long>
-        <max-offsets>   ::= <int>
+        <epoch>         ::= <int64>
+        <max-offsets>   ::= <int32>
 
-        The request-key (4) is encoded as a short.
+        The request-key (4) is encoded as a short (int16).
         """
         (topic, partition, offset, maxOffsets) = offsetRequest
-        req = struct.pack('>HH%dsiqi' % len(topic), RequestKey.OFFSET, len(topic), topic, partition, offset, maxOffsets)
+        req = struct.pack('>HH%dsiqi' % len(topic), KafkaClient.OFFSET_KEY, len(topic), topic, partition, offset, maxOffsets)
         return req
 
     def read_message_set(self, data):
@@ -245,7 +263,7 @@ class KafkaClient(object):
         Wire Format
         ===========
         <MessageSet> ::= <len> <Message> [ <MessageSet> ]
-        <len>        ::= <int>
+        <len>        ::= <int32>
 
         len is the length of the proceeding Message
         """
@@ -260,12 +278,14 @@ class KafkaClient(object):
                 if len(msgs) == 0:
                     raise Exception("Message underflow. Did not request enough bytes to consume a single message")
                 else:
+                    log.debug("Not enough data to read header of next message")
                     break
             # Read a Message header (length, magic byte)
             (N, magic) = struct.unpack('>iB', data[cur:(cur+5)])
 
             if (cur + N + 4) > size:
                 # Underflow for this Message
+                log.debug("Not enough data to read next message")
                 break
             cur += 5
 
@@ -276,6 +296,7 @@ class KafkaClient(object):
                 payload = data[cur:(cur+N-5)]
                 assert zlib.crc32(payload) == crc
                 cur += (N-5)
+                log.debug("Got v0 Message, %d bytes", len(payload))
                 msgs.append(Message(magic, None, crc, payload))
             elif magic == 1: # v1 Message
                 # Read attributes, crc; check the crc; append the message
@@ -284,14 +305,15 @@ class KafkaClient(object):
                 payload = data[cur:(cur+N-6)]
                 assert zlib.crc32(payload) == crc
                 cur += (N-6)
+                log.debug("Got v1 Message, %d bytes", len(payload))
                 msgs.append(Message(magic, att, crc, payload))
 
         # Return the retrieved messages and the cursor position
         return (msgs, cur)
 
-    ########################
-    #       User API       #  
-    ########################
+    #########################
+    #   Advanced User API   #  
+    #########################
 
     def send_message_set(self, produceRequest):
         """
@@ -302,6 +324,7 @@ class KafkaClient(object):
         produceRequest: ProduceRequest
         """
         req = length_prefix_message(self.create_produce_request(produceRequest))
+        log.debug("Sending %d bytes to Kafka", len(req))
         self._sock.send(req) 
 
     def send_multi_message_set(self, produceRequests):
@@ -313,6 +336,7 @@ class KafkaClient(object):
         produceRequests: list of ProduceRequest
         """
         req = length_prefix_message(self.create_multi_produce_request(produceRequests))
+        log.debug("Sending %d bytes to Kafka", len(req))
         self._sock.send(req) 
 
     def get_message_set(self, fetchRequest):
@@ -330,9 +354,9 @@ class KafkaClient(object):
         """
 
         req = length_prefix_message(self.create_fetch_request(fetchRequest))
+        log.debug("Sending %d bytes to Kafka", len(req))
         self._sock.send(req) 
         data = self._consume_response()
-
         (messages, read) = self.read_message_set(data)
 
         # Return the retrieved messages and the next FetchRequest
@@ -356,8 +380,10 @@ class KafkaClient(object):
         <MultiFetchResponse> ::= <MultiMessageSet>
         <MultiMessageSet>    ::= <MultiMessage> [ <MultiMessageSet> ]
         <MultiMessage>       ::= <len> 0 <MessageSet>
+        <len>                ::= <int32>
         """
         req = length_prefix_message(self.create_multi_fetch_request(fetchRequests))
+        log.debug("Sending %d bytes to Kafka", len(req))
         self._sock.send(req) 
         data = self._consume_response()
         cur = 0
@@ -385,12 +411,13 @@ class KafkaClient(object):
         Wire Format
         ===========
         <OffsetResponse> ::= <num> [ <offsets> ]
-        <num>            ::= <int>
+        <num>            ::= <int32>
         <offsets>        ::= <offset> [ <offsets> ]
-        <offset>         ::= <long>
+        <offset>         ::= <int64>
 
         """
         req = length_prefix_message(create_offset_request(offsetRequest))
+        log.debug("Sending %d bytes to Kafka", len(req))
         self._sock.send(req) 
 
         data = self._consume_response()
@@ -398,7 +425,10 @@ class KafkaClient(object):
         offsets = struct.unpack('>%dq' % num, data[4:])
         return offsets
 
-    
+    #######################
+    #   Simple User API   #
+    #######################
+
     def send_messages_simple(self, topic, partition, *payloads):
         """
         Send one or more strings to Kafka
@@ -412,14 +442,17 @@ class KafkaClient(object):
         messages = tuple([create_message_from_string(payload) for payload in payloads])
         self.send_message_set(ProduceRequest(topic, partition, messages))
 
-    def iter_messages(self, fetchRequest, auto=True):
+    def iter_messages(self, topic, partition, offset, size, auto=True):
         """
         Helper method that iterates through all messages starting at the offset
         in the given FetchRequest
 
         Params
         ======
-        fetchRequest: FetchRequest
+        topic: string
+        partition: int
+        offset: int, offset to start consuming from
+        size: number of bytes to initially fetch
         auto: boolean, indicates whether or not to automatically make the next 
               FetchRequest for more messages
 
@@ -427,6 +460,7 @@ class KafkaClient(object):
         =======
         A generator of Messages
         """
+        fetchRequest = FetchRequest(topic, partition, offset, size)
         while True:
             lastOffset = fetchRequest.offset
             (messages, fetchRequest) = self.get_message_set(fetchRequest)
