1b600a6a37b33035f05204bccefbf296dd4d8756
David Arthur <mumrah@gmail.com>
2019-10-24 14:31:37 -0400

    Add some inactive topics

--- tests/kafkatest/tests/core/replica_scale_test.py
+++ tests/kafkatest/tests/core/replica_scale_test.py
@@ -64,7 +64,7 @@ class ReplicaScaleTest(Test):
                                                 producer_conf={},
                                                 admin_client_conf={},
                                                 common_client_conf={},
-                                                inactive_topics={},
+                                                inactive_topics={"100k_replicas_bench[10-14]": {"numPartitions": 34, "replicationFactor": 3}},
                                                 active_topics={"100k_replicas_bench[0-4]": {"numPartitions": 34, "replicationFactor": 3}})
         produce_workload = self.trogdor.create_task("100k-replicas-produce-workload", produce_spec)
         produce_workload.wait_for_done(timeout_sec=3600)
