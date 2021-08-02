c4dc86bb78478b0ea819bdbca7dc535614ce5720
David Arthur <mumrah@gmail.com>
2021-08-02 11:49:55 -0400

    WIP

--- tests/kafkatest/tests/core/transactions_test.py
+++ tests/kafkatest/tests/core/transactions_test.py
@@ -246,7 +246,7 @@ class TransactionsTest(Test):
             check_order=[True, False],
             use_group_metadata=[True, False],
             metadata_quorum=quorum.all_non_upgrade)
-    def test_transactions(self, failure_mode, bounce_target, check_order, use_group_metadata, metadata_quorum=quorum.zk):
+    def test_transactions(self, failure_mode, bounce_target, check_order, use_group_metadata, metadata_quorum):
         security_protocol = 'PLAINTEXT'
         self.kafka.security_protocol = security_protocol
         self.kafka.interbroker_security_protocol = security_protocol
